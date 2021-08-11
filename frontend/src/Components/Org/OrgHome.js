a54719ec7f6c64df1d6f01a98ee5db4cbe5dd45f
Debashish-Dey-DM <deydebashish71@gmail.com>
2021-08-11 12:02:46 +0600

    restructured properly

---
+++ frontend/src/Components/Org/OrgHome.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+ const OrgHome = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+export default OrgHome;
---
+++ frontend/src/Components/Sponsor/SponsorHome.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+ const SponsorHome = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+export default SponsorHome;
