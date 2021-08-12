d690ea0a22ac4af7c34ad9ff4bb9c061a91098d0
Debashish-Dey-DM <deydebashish71@gmail.com>
2021-08-12 17:35:08 +0600

    test component

--- frontend/src/Components/Org/OrgHome.js
+++ frontend/src/Components/Org/OrgHome.js
@@ -1,8 +1,9 @@
 import React from 'react'
-
+import { Link } from 'react-router-dom';
  const OrgHome = () => {
     return (
         <div>
+            <Link to={'/org/createEvent'} className="btn btn-primary btn-sm foat-end"> Create New Event</Link>
             
         </div>
     )
