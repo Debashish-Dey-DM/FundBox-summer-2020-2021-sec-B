4b1c5fd5259b859d32f26a5179ffddeefd648f80
Riaz <almahmudr1@gmail.com>
2021-08-19 04:21:38 +0600

    Admin Home done

--- frontend/src/Components/Admin/AdminHome.js
+++ frontend/src/Components/Admin/AdminHome.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import axios from 'axios';
 import { Link } from 'react-router-dom';
 import logo from '../../dummyLogo.png';
 import avatar from '../../avatar.png';
 import LeftNavBar from './Layout/LeftNavBar';
 import TopNavbar from './Layout/TopNavbar';
+import { useState,useEffect} from 'react';
+
 
 // import 'boxicons';
 
@@ -23,6 +26,19 @@ import TopNavbar from './Layout/TopNavbar';
         // console.log(JSON.parse(data))
         console.log(JSON.parse(setData))
     }
+
+    const [event, setEvent] = useState([]);
+    const mount= async()=>{
+        const res = await axios.get('http://localhost:8000/api/admin/dashboard');
+        console.log(res.data);
+        
+        if (res.status === 200) {
+            setEvent(res.data)
+        }
+    }
+    useEffect(() => {
+        mount();    
+    }, []);
 
     return (
         <div className="sb-nav-fixed">
@@ -47,7 +63,7 @@ import TopNavbar from './Layout/TopNavbar';
                                                     <i className="bx bx-receipt font-medium-5"></i>
                                                 </div>
                                                 <div className="text-muted line-ellipsis">Total Site Visit</div>
-                                                <h3 className="mb-0">100</h3>
+                                                <h3 className="mb-0">{event.totalSiteVisit}</h3>
                                             </div>
                                         </div>
                                     </div>
@@ -60,7 +76,7 @@ import TopNavbar from './Layout/TopNavbar';
                                                 <i className="bx bx-receipt font-medium-5"></i>
                                                 </div>
                                                 <div className="text-muted line-ellipsis">Unique IP</div>
-                                                <h3 className="mb-0">100</h3>
+                                                <h3 className="mb-0">{event.uniqueIp}</h3>
                                             </div>
                                         </div>
                                     </div>
@@ -84,7 +100,7 @@ import TopNavbar from './Layout/TopNavbar';
                                                     <i className="bx bxs-smiley-happy font-medium-5"></i>
                                                 </div>
                                                 <div className="text-muted line-ellipsis">Total Collect</div>
-                                                <h3 className="mb-0">৳ 100</h3>
+                                                <h3 className="mb-0">৳ {event.totalMoneyCollect}</h3>
                                             </div>
                                         </div>
                                     </div>
@@ -97,7 +113,7 @@ import TopNavbar from './Layout/TopNavbar';
                                                     <i className="bx bxs-smiley-sad font-medium-5"></i>
                                                 </div>
                                                 <div className="text-muted line-ellipsis">Refund Money</div>
-                                                <h3 className="mb-0">৳ 100</h3>
+                                                <h3 className="mb-0">৳ {event.refundMoney}</h3>
                                             </div>
                                         </div>
                                     </div>
@@ -110,7 +126,7 @@ import TopNavbar from './Layout/TopNavbar';
                                                     <i className="bx bxs-smiley-happy font-medium-5"></i>
                                                 </div>
                                                 <div className="text-muted line-ellipsis">Today Collect</div>
-                                                <h3 className="mb-0">৳ 100</h3>
+                                                <h3 className="mb-0">৳ {event.todayCollect}</h3>
                                             </div>
                                         </div>
                                     </div>
@@ -123,7 +139,7 @@ import TopNavbar from './Layout/TopNavbar';
                                                     <i className="bx bxs-smiley-sad font-medium-5"></i>
                                                 </div>
                                                 <div className="text-muted line-ellipsis">Today Refund Money</div>
-                                                <h3 className="mb-0">৳ 100</h3>
+                                                <h3 className="mb-0">৳ {event.todayRefund}</h3>
                                             </div>
                                         </div>
                                     </div>
@@ -147,7 +163,7 @@ import TopNavbar from './Layout/TopNavbar';
                                                     <i className="bx bx-receipt font-medium-5"></i>
                                                 </div>
                                                 <div className="text-muted line-ellipsis">Total Event</div>
-                                                <h3 className="mb-0">100</h3>
+                                                <h3 className="mb-0">{event.totalEvents}</h3>
                                             </div>
                                         </div>
                                     </div>
@@ -160,7 +176,7 @@ import TopNavbar from './Layout/TopNavbar';
                                                     <i className="bx bxs-error font-medium-5"></i>
                                                 </div>
                                                 <div className="text-muted line-ellipsis">Pending Event</div>
-                                                <h3 className="mb-0">100</h3>
+                                                <h3 className="mb-0">{event.pendingEvents}</h3>
                                             </div>
                                         </div>
                                     </div>
@@ -173,7 +189,7 @@ import TopNavbar from './Layout/TopNavbar';
                                                     <i className="bx bxs-smiley-happy font-medium-5"></i>
                                                 </div>
                                                 <div className="text-muted line-ellipsis">Accepted Event</div>
-                                                <h3 className="mb-0">100</h3>
+                                                <h3 className="mb-0">{event.acceptedEvents}</h3>
                                             </div>
                                         </div>
                                     </div>
@@ -186,7 +202,7 @@ import TopNavbar from './Layout/TopNavbar';
                                                     <i className="bx bxs-smiley-sad font-medium-5"></i>
                                                 </div>
                                                 <div className="text-muted line-ellipsis">Deactivate Event</div>
-                                                <h3 className="mb-0">100</h3>
+                                                <h3 className="mb-0">{event.deactiveEvents}</h3>
                                             </div>
                                         </div>
                                     </div>
@@ -210,7 +226,7 @@ import TopNavbar from './Layout/TopNavbar';
                                                     <i className="bx bx-user font-medium-5"></i>
                                                 </div>
                                                 <div className="text-muted line-ellipsis">Admins</div>
-                                                <h3 className="mb-0">100</h3>
+                                                <h3 className="mb-0">{event.totalAdmin}</h3>
                                             </div>
                                         </div>
                                     </div>
@@ -223,7 +239,7 @@ import TopNavbar from './Layout/TopNavbar';
                                                     <i className="bx bx-user font-medium-5"></i>
                                                 </div>
                                                 <div className="text-muted line-ellipsis">Organization</div>
-                                                <h3 className="mb-0">100</h3>
+                                                <h3 className="mb-0">{event.totalOrg}</h3>
                                             </div>
                                         </div>
                                     </div>
@@ -236,7 +252,7 @@ import TopNavbar from './Layout/TopNavbar';
                                                     <i className="bx bx-user font-medium-5"></i>
                                                 </div>
                                                 <div className="text-muted line-ellipsis">Sponsor</div>
-                                                <h3 className="mb-0">100</h3>
+                                                <h3 className="mb-0">{event.totalSpo}</h3>
                                             </div>
                                         </div>
                                     </div>
@@ -249,7 +265,7 @@ import TopNavbar from './Layout/TopNavbar';
                                                     <i className="bx bx-user font-medium-5"></i>
                                                 </div>
                                                 <div className="text-muted line-ellipsis">User</div>
-                                                <h3 className="mb-0">100</h3>
+                                                <h3 className="mb-0">{event.totalUser}</h3>
                                             </div>
                                         </div>
                                     </div>
